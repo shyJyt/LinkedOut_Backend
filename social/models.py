07f77c31219a1896f5ae78fec63de89efe9da4b7
iwacat <hanxy2001@163.com>
2024-06-23 19:14:18 +0800

    update: add models in social

--- social/models.py
+++ social/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from user.models import User
+from enterprise.models import Enterprise
+
 
 # Create your models here.
+class UserActivity(models.Model):
+    user_id = models.ForeignKey(to='user.User', on_delete=models.CASCADE)
+    from_user_act_id = models.ForeignKey(to='UserActivity', null=True)
+    from_enter_user_act_id = models.ForeignKey(to='EnterpriseUserActivity', null=True)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    like = models.ManyToManyField(to='user.User', related_name='like_user_activity', on_delete=models.CASCADE)
+
+
+class EnterpriseUserActivity(models.Model):
+    user_id = models.ForeignKey(to='user.User', on_delete=models.CASCADE)
+    enter_id = models.ForeignKey(to='enterprise.Enterprise', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    like = models.ManyToManyField(to='user.User', related_name='like_user_activity', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(to='user.User', on_delete=models.CASCADE)
+    user_act_id = models.ForeignKey(to='UserActivity', null=True)
+    enter_user_act_id = models.ForeignKey(to='EnterpriseUserActivity', null=True)
+    content = models.TextField()
+
+
+class Message(models.Model):
+    from_user_id = models.ForeignKey(to='user.User', on_delete=models.CASCADE)
+    to_user_id = models.ForeignKey(to='user.User', on_delete=models.CASCADE)
+    type = models.IntegerField(choices=((0, '系统消息'), (1, '点赞'), (2, '评论'), (3, '转发'), (4, '关注')), default=0)
+    obj_id = models.CharField(max_length=10)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+
