621337043d75757bb02eace2dc5130a205127bb8
zh <1812643720@qq.com>
2024-06-26 23:50:06 +0800

    feat: modify

--- LinkedOut_Backend/asgi.py
+++ LinkedOut_Backend/asgi.py
@@ -8,5 +8,25 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+from channels.routing import get_default_application
+
+
+django.setup()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LinkedOut_Backend.settings")
+
+application = get_default_application()
+
+# application = ProtocolTypeRouter({
+#     # http请求使用这个
+#     "http": get_asgi_application(),
+#
+#     # websocket请求使用这个
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             websocket_urlpatterns
+#         )
+#     ),
+# })
--- LinkedOut_Backend/settings.py
+++ LinkedOut_Backend/settings.py
@@ -163,6 +163,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = BUAA_MAIL_USER
 EMAIL_HOST_PASSWORD = BUAA_MAIL_TOKEN
 
+
 ASGI_APPLICATION = "LinkedOut_Backend.routing.application"
 CHANNEL_LAYERS = {
     # "default": {
